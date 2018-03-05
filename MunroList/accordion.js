753234194d8432b11cff09a73a0c61f201dfb457
Katie Dear <Katie@Katies-MacBook-Air.local>
2018-03-05 15:34:55 +0000

    Saved changes

--- MunroList/accordion.js
+++ MunroList/accordion.js
@@ -2,49 +2,54 @@ var acc = document.getElementsByClassName("accordion");
 var panel = document.getElementsByClassName('panel');
 var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function() {
-        var setClasses = !this.classList.contains('active');
-        setClass(acc,'active','remove');
-        setClass(panel,'show','remove');
+ for (i = 0; i < acc.length; i++) {
+     acc[i].onclick = function() {
+         var setClasses = !this.classList.contains('active');
+         setClass(acc,'active','remove');
+         setClass(panel,'show','remove');
 
-        if (setClasses) {
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
-    }
-}
+         if (setClasses) {
+             this.classList.toggle("active");
+             this.nextElementSibling.classList.toggle("show");
+         }
+     }
+ }
 
-function setClass(els,className,fnName) {
-    for (i = 0; i < els.length; i++) {
-        els[i].classList[fnName](className);
-    }
-}
+ function setClass(els,className,fnName) {
+     for (i = 0; i < els.length; i++) {
+         els[i].classList[fnName](className);
+     }
+ }
 
-$(document).ready(function() {
-
-  $(acc).click(function() {
-    var displayMunros = $(panel);
-
-    $.ajax( {
-      type: "GET",
-      url: "munrodata.json",
-      success: function(result)
-      {
-        console.log(result.munros);
-        var munros = result.munros;
-
-        var output = "<table><tbody>";
-        for (var i in munros)
-        {
-          output+="<tr><td>" + munros[i].name + "</td><td>"
-          + munros[i].region + "</td><td>" + munros[i].height + "</td></tr>";
-        }
-        output+="</tbody></table>";
-
-        displayMunros.html(output);
-        $("table").addClass("table");
-      }
-    });
+$(function() {
+  $('.accordion').button(function() {
+    addItemToList("example item");
+    return false;
   });
 });
+
+function addItemToList(item) {
+  $('.results').append("<ul>" + item + "</ul>");
+}
+
+function getResultsFromDB(searchterms) {
+  var url = "munrodata.json" + searchterms;
+  $,getJSON(url, function(jsondata) {
+    // prettyPrintJSON(jsondata);
+    addResults(jsondata);
+  });
+}
+
+// function prettyPrintJSON(jsondata) {
+//   var pretty = JSON.stringify(jsondata, null, 4);
+//   $('.panel').append("<a>" + pretty + "</a>")
+// }
+
+function addResults(jsondata) {
+  var htmlstring = "";
+  for(var i = 0; i < 10; i++) {
+    var munro = jsondata.Search[i].Munro;
+    htmlstring += "<ul>" + name + "</ul>";
+  }
+  $(".results").html(htmlstring);
+}
