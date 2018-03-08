498f036a127d9c93f355754fa3da063032cffb4b
1601156 <katiedear@btinternet.com>
2018-03-08 12:37:21 +0000

    Accordion changes

--- MunroList/accordion.js
+++ MunroList/accordion.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
 
         for(var i =0;i<munros.length;i++)
         {
-          var output = "<h3>" + munros[i].name + "</h3>";
+          var output = "<h4>" + munros[i].name + "</h4>";
           output += "<div class='whiteback'><p>Region: " + munros[i].region + "</p><p>Height: " + munros[i].height + "</p><p>Latitude: " + munros[i].latitude + "</p><p>Longitude: " + munros[i].longitude + "</p><p>Grid Reference: " + munros[i].gridReference + "</p>";
           output += "</div>";
           displayMunros.append(output);
