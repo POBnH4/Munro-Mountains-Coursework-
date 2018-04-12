aa8c769514d83a01a2df2c5fd7ba66e102d57880
Katie Dear <Katie@Katies-MacBook-Air.local>
2018-04-12 11:11:36 +0100

    Saved changes

--- Coursework/list/accordion.js
+++ Coursework/list/accordion.js
@@ -1,3 +1,11 @@
+function listenerEx(i) {
+    if($("#checkBox" + i).is(":checked")) {
+        $("#bMount" + i).show();
+    } else {
+        $("#bMount" + i).hide();
+    }
+}
+
 $(document).ready(function() {
     var displayMunros = $('#accordion');
 
@@ -11,7 +19,7 @@ $(document).ready(function() {
 
         for(var i = 0; i < munros.length; i++)
         {
-          var output = "<h4 class='munrotitle'>" + munros[i].name;
+          var output = "<h4 class='munrotitle'>" + munros[i].name + "<img class='bMount' id='bMount" + i + "' src='Mountain_Pins/blueMnt.png'>";
 
           output += "</h4><div class='whiteback'><table><tr><td>Description: </td><td>" + munros[i].description + "</td></tr><tr><td>Region: </td><td>" + munros[i].region + "</td></tr><tr><td>Height: </td><td>" + munros[i].height + "</td></tr><tr><td>Latitude: </td><td>" + munros[i].latitude + "</td></tr><tr><td>Longitude: </td><td>" + munros[i].longitude + "</td></tr><tr><td>Grid Reference: </td><td>" + munros[i].gridReference + "</td></tr><tr><td>Difficulty: </td><td>";
 
@@ -23,57 +31,51 @@ $(document).ready(function() {
                 output += "<img src='Mountain_Pins/redMnt.png'><img src='Mountain_Pins/redMnt.png'><img src='Mountain_Pins/redMnt.png'>";
             }
 
-          output += "</td></tr><tr><td>Climbed: </td></tr><tr><td>";
-
-          function DoCheckUncheckDisplay(d,dchecked,dunchecked)
-          {
-            if( d.checked == true )
-            {
-              document.getElementById(dchecked).style.display = "block";
-              document.getElementById(dunchecked).style.display = "none";
-            }
-            else
-            {
-              document.getElementById(dchecked).style.display = "none";
-              document.getElementById(dunchecked).style.display = "block";
-            }
-          }
-
-          output += "<div>Check/uncheck:<input type="checkbox" onclick="DoCheckUncheckDisplay(this,'checkbox-checked','checkbox-unchecked')" style="margin:0;"></div><div id="checkbox-checked" style="display:none; color:green;">The checkbox is checked.</div><div id="checkbox-unchecked" style="display:none; color:red;">The checkbox is unchecked.</div>";
-          output += "</td></tr></table></div>";
+          output += "</td></tr><tr><td>Climbed: </td><td><input type='checkbox' id='checkBox" + i + "' onclick='listenerEx(" + i + ")'></td></tr></table></div>";
+            
+            currentFunction = function() {
+                if($("#checkBox" + i).is(":checked")) {
+                    $("#bMount" + i).show();
+                    console.log("checked");
+                } else {
+                    console.log("unchecked");
+                    $("#bMount" + i).hide();
+                }
+            };
+            
+//            console.log("creating listener for " + i);
+            $("#checkBox" + i).click(function() {
+                currentFunction();
+                console.log(i);
+            });
+            
           displayMunros.append(output);
         }
-        $( "#accordion" ).accordion({collapsible:true});
+        $( "#accordion" ).accordion({collapsible:true, active: false});
+        $(".bMount").hide();
       }
   });
 });
 
-
-// $(function myFunction() {
-//     var x = document.getElementById("checkBox");
-//     if (x.checked == true) {
-//         output += "<img src='Mountain_Pins/blueMnt.png'>";
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// });
-
-
-
-// function getSortOrder(prop) {
-//   return function(a, b) {
-//     if(a[prop] > b[prop]) {
-//       return 1;
-//     } else if (a[prop] < b[prop]) {
-//       return -1;
-//     }
-//     return 0;
-//   }
-// }
-// $('#height').click(function() {
-//   munros.sort(getSortOrder("height"));
-//   for(var items in munros) {
-//     document.write("<div>" + munros[item].height + "</div>");
-//   }
-// });
+function myFunction() {
+    // Declare variables
+    var input, filter, h4, div, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    h4 = document.getElementsByTagName("h4");
+//    acc = document.getElementById("accordion");
+    div = document.getElementsByClassName("whiteback");
+//    table = div.getElementsByTagName("table");
+    
+    // Loop through all munros, and hide those that don't match the search query
+    // DO NOT CHANGE
+    for(i = 0; i < h4.length; i++) {
+        a = h4[i];
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            h4[i].style.display = "";
+        } else {
+            h4[i].style.display = "none";
+        }
+    }
+    // DO NOT CHANGE
+}
