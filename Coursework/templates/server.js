067ec7e4b42ca721a8293e8a5fa25ecbaf04d12c
DReid98 <d.a.reid@rgu.ac.uk>
2018-04-12 10:47:19 +0100

    Templates and Header updated

--- Coursework/templates/server.js
+++ Coursework/templates/server.js
@@ -21,8 +21,29 @@ app.get('/', function(req,res) {
     res.render('pages/index');
 });
 
+// map page
 app.get('/munromap', function(req,res) {
     res.render('pages/map');
+});
+
+// list page
+app.get('/munrolist', function(req,res) {
+    res.render('pages/list');
+});
+
+// info page
+app.get('/information', function(req,res) {
+    res.render('pages/info');
+});
+
+// contact page
+app.get('/contact', function(req,res) {
+    res.render('pages/contact');
+});
+
+// login page
+app.get('/login', function(req,res) {
+    res.render('pages/login');
 });
 
 app.listen(8080);
