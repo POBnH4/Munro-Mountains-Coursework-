c3d2067493ab70a7054ea60681d167ca4186a03a
DReid98 <d.a.reid@rgu.ac.uk>
2018-04-13 10:09:10 +0100

    MongoDB creation and map testing - attempt to load munro data from mongodb

--- Coursework/templates/server.js
+++ Coursework/templates/server.js
@@ -11,7 +11,7 @@ var app = express();
 
 //Required packages
 const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
-const url = "mongodb://localhost:27017/profiles";
+const url = "mongodb://localhost:27017/munrospotter";
 const express = require('express'); //npm install express
 const session = require('express-session'); //npm install express-session
 const bodyParser = require('body-parser'); //npm install body-parser
