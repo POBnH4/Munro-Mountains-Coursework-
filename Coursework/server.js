1a5b5c80cf2b2731414d710f28d81f3468a99d60
Peter Bonchev <bonchp4@yahoo.com>
2018-04-20 13:42:49 +0100

    another Test

--- Coursework/server.js
+++ Coursework/server.js
@@ -8,7 +8,7 @@ const app = express();
 const USER_DOES_NOT_EXIST = 0, USER_EXISTS = 1;
 
 const USERNAME_VALIDITY = new RegExp("[a-zA-Z]");
-const EMAIL_VALIDITY = new RegExp("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+//const EMAIL_VALIDITY = new RegExp("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
 const PASSWORD_VALIDITY = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,20})");
 //the password must contain at least one lowercase letter,
 // one uppercase letter, one digit, and be between 8 and 20 characters;
